5a45eec98e1a27aa1e27de270c5e86929405a94b
unknown <>
2016-07-08 17:12:43 +0900

    add angularjs

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var basic = require('./routes/basic');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/basic', basic);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- module_EXPRESS.js
+++ module_EXPRESS.js
@@ -21,36 +21,9 @@ function start(){
     //console.log(req.route);
   })
 
-  app.post('/', function (req, res) {
-    res.render('jade_test', {temps: 'Express123123'})
-    //res.sendFile(__dirname + '/Route_Page.html',{ temps: 'Express123123'});
-    //console.log(req.body.flgs);
-  })
-
-  app.get('/message/:id', function(req, res){
-    var id = req.params.id
-    var str = require('crypto')
-    .createHash('sha1')
-    .update(new Date().toDateString() + id)
-    .digest('hex')
-    res.send(str)
-  })
-
-  app.get('/search', function(req, res){
-    var query = req.query
-    res.send(query)
-  })
-
-  app.get('/books', function(req, res){
-    fs.readFile(__dirname +'/books.json', function(e, data) {
-        if (e) return res.sendStatus(500)
-        try {
-          books = JSON.parse(data)
-        } catch (e) {
-          res.sendStatus(500)
-        }
-        res.json(books)
-      })
+  app.get('/base', function (req, res) {
+    res.sendFile(__dirname + '/Route_Page.html');
+    //console.log(req.route);
   })
 
   app.listen(3000, function () {
