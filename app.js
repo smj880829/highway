c8b2011d020afbc44237b3668bcf552f1ef01088
unknown <>
2016-07-08 17:46:50 +0900

    a

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ var http = require('http');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var basic = require('./routes/basic');
+var addsomecontrol = require('./routes/add_some_control');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/basic', basic);
+app.use('/add_some_control', addsomecontrol);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
