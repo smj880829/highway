cc905602d0fd45814a2ef88b99ce54b97d6a84dd
unknown <>
2016-11-06 08:59:20 +0900

    wow

--- mysqlConnector/connector.js
+++ mysqlConnector/connector.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: '52.78.102.96',
     user: 'root',
     database: 'highway1',
     password: 'root'
